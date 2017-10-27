c1fb08d847968b8867ca30447e5a6fcbd8474755
Victor Diego <victordieggo@gmail.com>
2017-10-27 13:34:08 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Spaceman #
+
+Spaceman is a simple and easily customizable HTML/CSS boilerplate to build websites and apps. It provides a flexbox based responsive layout system, pre-set styles for basic UI elements and a few CSS utilities and components.
+
+### Getting Started ###
+
+Spaceman uses [Gulp](http://gulpjs.com/) and [Browsersync](https://www.browsersync.io/), so you'll need [Node.JS](https://nodejs.org/en/). Once you have set it up, [download Spaceman](https://github.com/victordieggo/spaceman/releases/latest) to your project directory and access the terminal:
+
+```
+# install gulp globally
+$ npm install -g gulp
+
+# go to your project directory
+$ cd myproject
+
+# install dev dependencies:
+$ npm install
+
+# initialize gulp and browsersync:
+$ gulp
+```
+
+### Gulp Tasks ###
+
+- `gulp`: run all tasks and start browsersync
+- `gulp css`: compile css files
+- `gulp js`: compile js files
+- `gulp img`: compress images
+- `gulp svg`: minify svg files
+- `gulp watch`: start browsersync
+
+### Browser Support ###
+
+Chrome 29+, IE 10+, Firefox 22+, Safari 6.1+, Opera 12+
+
+### License ###
+
+[MIT License](https://victordieggo.mit-license.org/) © Victor Diego Villar Guimarães
---
+++ assets/src/js/navigation.js
@@ -0,0 +1,91 @@
+/* =====================================================================
+ * SPACEMAN MOBILE NAVIGATION
+ * ===================================================================*/
+/*global window, document*/
+
+(function () {
+
+    'use strict';
+
+    var screenCover,
+        navIsActive  = 'main-nav-is-active',
+        itemIsActive = 'menu-item-is-active',
+        body         = document.body,
+        navBtn       = body.querySelector('.nav-btn'),
+        nav          = body.querySelector('.main-nav'),
+        parentItem   = body.querySelectorAll('.menu-item-has-children');
+
+    function lockScreen() {
+        body.classList.toggle('nav-hide-overflow');
+        if (nav.contains(screenCover)) {
+            nav.removeChild(screenCover);
+        } else {
+            screenCover = document.createElement('div');
+            screenCover.className = 'screen-cover';
+            nav.appendChild(screenCover);
+        }
+    }
+
+    function toggleExpanded(element) {
+        var ariaExpanded = element.getAttribute('aria-expanded');
+        element.setAttribute('aria-expanded', ariaExpanded === 'false' ? 'true' : 'false');
+    }
+
+    function toggleNavigation() {
+        nav.classList.toggle(navIsActive);
+        toggleExpanded(navBtn);
+        lockScreen();
+    }
+
+    function toggleSubmenu(element) {
+        element.querySelector('a').addEventListener('click', function (event) {
+            event.preventDefault();
+            element.classList.toggle(itemIsActive);
+            toggleExpanded(this);
+        });
+    }
+
+    Array.prototype.forEach.call(['click', 'keyup'], function (event) {
+        body.addEventListener(event, function (event) {
+            var type = event.type,
+                key = event.keyCode,
+                target = event.target,
+                onFocus = nav.querySelector(':focus'),
+                activeItems = nav.querySelectorAll('.' + itemIsActive),
+                closeActiveItem = function (activeItem) {
+                    activeItem.classList.remove(itemIsActive);
+                    toggleExpanded(activeItem.querySelector('a'));
+                },
+                clickExceptions = [
+                    nav.querySelector('.menu'),
+                    screenCover,
+                    navBtn
+                ];
+            if (type === 'click') {
+                if (target === navBtn || target === screenCover) {
+                    toggleNavigation();
+                }
+            }
+            Array.prototype.forEach.call(activeItems, function (activeItem) {
+                if (type === 'click' && !activeItem.contains(target)) {
+                    var activeNav = nav.contains(screenCover),
+                        browserWidth = window.innerWidth,
+                        exceptions = clickExceptions.indexOf(target) === -1;
+                    if (browserWidth > 992 || (browserWidth <= 992 && exceptions && activeNav)) {
+                        closeActiveItem(activeItem);
+                    }
+                }
+                if (type === 'keyup') {
+                    if (key === 27 || (key === 9 && !activeItem.contains(onFocus))) {
+                        closeActiveItem(activeItem);
+                    }
+                }
+            });
+        });
+    });
+
+    Array.prototype.forEach.call(parentItem, function (element) {
+        toggleSubmenu(element);
+    });
+
+}());
